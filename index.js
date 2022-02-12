411f3d20cd9a85ff2b1df97369ae76d6942384d9
MofasserHossain <mofasserhossain1221@gmail.com>
2022-02-12 20:43:33 +0600

    route added

--- index.js
+++ index.js
@@ -13,6 +13,13 @@ app.use(express.urlencoded({ extended: true }));
 // publisher
 app.get('/rtcToken', generateRtcToken);
 
+app.get('/', (req, res) => {
+  res.status(200).json({
+    status: 'OK',
+    message: 'Server is running...',
+  });
+});
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`listening to port ${port}`);
